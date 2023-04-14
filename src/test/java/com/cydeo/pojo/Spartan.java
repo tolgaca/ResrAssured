5de3b4dd45e874e3fa6a343193c6d10599bd008a
Tolga Can Arık <tolga.c.arik@gmail.com>
2023-04-14 18:11:19 +0300

    Pojo

---
+++ src/test/java/com/cydeo/pojo/Spartan.java
@@ -0,0 +1,23 @@
+package com.cydeo.pojo;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+@JsonIgnoreProperties(value = "id",allowSetters = true)
+public class Spartan {
+        //getter setter
+        //toString
+        private int id;
+        private String name;
+        private String gender;
+        private long phone;
+
+
+}
